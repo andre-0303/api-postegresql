edf8051e0f383e7cc90ac383359456cc200f4c41
andre-0303 <bandeiradev.ti@gmail.com>
2025-05-13 20:10:52 -0300

    :sparkles: Adicionando rota de deletar

--- index.js
+++ index.js
@@ -61,6 +61,22 @@ app.put("/alunos/:id", async (req, res) => {
   }
 });
 
+app.delete("/alunos/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await pool.query(
+      "DELETE FROM alunos WHERE id = $1 RETURNING *",
+      [id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Aluno não encontrado" });
+    }
+    res.json({ message: "Aluno deletado com sucesso", aluno: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
